6140c701abf7d7e7d53bb6e5b3f77e6da14c8e7c
Zach Tullis <zachjtullis@gmail.com>
2024-04-08 22:34:15 -0600

    Add project files.

---
+++ 2-4AurorasBricks2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2_4AurorasBricks2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
